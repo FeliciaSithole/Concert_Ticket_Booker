7da5daf6850a207af0a6666c198f5bb6028c6f5b
FeliciaSithole <feliciasithole.fs@gmail.com>
2026-07-17 00:16:23 +0200

    added personalized confirmation message

--- concert_ticket_booker.py
+++ concert_ticket_booker.py
@@ -2,3 +2,4 @@ user_name = input("Enter your name: ")
 
 artist_name = input("Enter the artist or band you eant to see: ")
 
+print(f"\nHey {user_name}! Your tickets to see {artist_name} are booked successfully!")
